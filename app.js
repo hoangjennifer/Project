ac7dcbc4e78519c329c2336461d07223037da1e5
hoangjennifer <63030751+hoangjennifer@users.noreply.github.com>
2023-03-01 23:09:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,42 +4,97 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app = express();            // We need to instantiate an express object to interact with the server in our code
-PORT = 10200;                 // Set a port number at the top so it's easy to change in the future
+// Configure Express to Handle JSON and Form Data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+PORT = 10201;                 // Set a port number at the top so it's easy to change in the future
+// Express Handlebars
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 // Database
 var db = require('./database/db-connector')
 
 /*
     ROUTES
 */
+
 app.get('/', function (req, res) {
-    // Define our queries
-    query1 = 'DROP TABLE IF EXISTS diagnostic;';
-    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-    query4 = 'SELECT * FROM diagnostic;';
+    let query1 = "SELECT * FROM Users;";               // Define our query
 
-    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+    db.pool.query(query1, function (error, rows, fields) {    // Execute the query
+        res.render('index', { data: rows });                  // Render the index.hbs file, and also send the renderer
+    })                                                      // an object where 'data' is equal to the 'rows' we
+});                                                         // received back from the query
 
-    // DROP TABLE...
-    db.pool.query(query1, function (err, results, fields) {
+app.post('/add-user-ajax', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
 
-        // CREATE TABLE...
-        db.pool.query(query2, function (err, results, fields) {
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Users (name, email) VALUES ('${data.name}', '${data.email}')`;
+    db.pool.query(query1, function (error, rows, fields) {
 
-            // INSERT INTO...
-            db.pool.query(query3, function (err, results, fields) {
+        // Check to see if there was an error
+        if (error) {
 
-                // SELECT *...
-                db.pool.query(query4, function (err, results, fields) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Users;`;
+            db.pool.query(query2, function (error, rows, fields) {
 
-                    // Send the results to the browser
-                    let base = "<h1>MySQL Results:</h1>"
-                    res.send(base + JSON.stringify(results));
-                });
-            });
-        });
-    });
+                // If there was an error on the second query, send a 400
+                if (error) {
+
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else {
+                    res.send(rows);
+                }
+            })
+        }
+    })
 });
+
+app.delete('/delete-user-ajax/', function (req, res, next) {
+    let data = req.body;
+    let userID = parseInt(data.userID);
+    let deleteTasks = `DELETE FROM Tasks WHERE userID = ?`;
+    let deleteUsers = `DELETE FROM Users WHERE userID = ?`;
+
+
+    // Run the 1st query
+    db.pool.query(deleteTasks, [userID], function (error, rows, fields) {
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        }
+
+        else
+            {
+                // Run the second query
+                db.pool.query(deleteUsers, [userID], function(error, rows, fields) {
+
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                })
+            }
+})});
 
 /*
     LISTENER
